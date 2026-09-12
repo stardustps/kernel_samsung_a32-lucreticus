97359380d782bd0300826c3f2c4989470570c7fa
dotarma <ncatt@tuta.io>
2026-09-12 14:20:10 +0000

    fs: integrate nomount hooks to hide mountpoints
    Hook do_mount and do_umount in fs/namespace.c to route mount and unmount requests through nomount policy checks.
    
    Filter hidden mountpoints in fs/proc_namespace.c show_vfsmnt, show_mountinfo and show_vfsstat so sensitive paths are skipped in mounts, mountinfo and mountstats output.
    
    Add fs/nomount.c with substring-based hiding, path checks and a /proc/nomount_enabled control node, wire it into fs/Kconfig and fs/Makefile, and enable CONFIG_NOMOUNT in mt6768_lucreticus_defconfig.
    
    Signed-off-by: dotarma <ncatt@tuta.io>

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -17,6 +17,7 @@
 
 #include "pnode.h"
 #include "internal.h"
+#include "nomount.h"
 
 static unsigned mounts_poll(struct file *file, poll_table *wait)
 {
@@ -102,6 +103,10 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	struct super_block *sb = mnt_path.dentry->d_sb;
 	int err;
 
+#ifdef CONFIG_NOMOUNT
+	if (nomount_should_hide_mnt(mnt))
+		return SEQ_SKIP;
+#endif
 	if (sb->s_op->show_devname) {
 		err = sb->s_op->show_devname(m, mnt_path.dentry);
 		if (err)
@@ -138,6 +143,10 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	struct path mnt_path = { .dentry = mnt->mnt_root, .mnt = mnt };
 	int err;
 
+#ifdef CONFIG_NOMOUNT
+	if (nomount_should_hide_mnt(mnt))
+		return SEQ_SKIP;
+#endif
 	seq_printf(m, "%i %i %u:%u ", r->mnt_id, r->mnt_parent->mnt_id,
 		   MAJOR(sb->s_dev), MINOR(sb->s_dev));
 	if (sb->s_op->show_path) {
@@ -202,6 +211,10 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	struct super_block *sb = mnt_path.dentry->d_sb;
 	int err;
 
+#ifdef CONFIG_NOMOUNT
+	if (nomount_should_hide_mnt(mnt))
+		return SEQ_SKIP;
+#endif
 	/* device */
 	if (sb->s_op->show_devname) {
 		seq_puts(m, "device ");
